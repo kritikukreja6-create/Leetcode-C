60d902343e31de1abbcb0f6172eee9345408cae3
kriti kukreja <kritikukreja6@gmail.com>
2026-01-03 22:56:14 +0530

    Time: 0 ms (100%), Space: 7.7 MB (81.87%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.c
@@ -0,0 +1,20 @@
+bool isPerfectSquare(int num) {
+    if( num < 1) return false;
+    if (num == 1) return true;
+
+    long low = 1; 
+    long high = num;
+     while ( low <= high) {
+        long mid = low + (high - low) / 2;
+        long square = mid * mid;
+
+        if (square == num) {
+            return true;
+        } else if ( square < num) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+     }
+    return false;
+}
