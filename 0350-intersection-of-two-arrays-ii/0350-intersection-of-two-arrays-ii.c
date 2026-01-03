5db989f330faf2596a9359ead340d96e49acd0b8
kriti kukreja <kritikukreja6@gmail.com>
2026-01-03 22:45:22 +0530

    Time: 3 ms (40.13%), Space: 10 MB (57.92%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.c
@@ -0,0 +1,29 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+ int compare(const void*a , const void*b) {
+    return (*(int*)a - *(int*)b);
+ }
+int* intersect(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize) {
+    qsort(nums1 , nums1Size , sizeof(int) , compare);
+    qsort(nums2 , nums2Size , sizeof(int) , compare);
+
+    int maxSize = (nums1Size < nums2Size) ? nums1Size : nums2Size;
+    int* result = (int*)malloc(maxSize * sizeof(int));
+    int i = 0 , j = 0 , k = 0;
+
+    while(i < nums1Size && j < nums2Size) {
+        if (nums1[i] == nums2[j]) {
+            result[k++] = nums1[i];
+            i++;
+            j++;
+        } else if (nums1[i] < nums2[j]) {
+            i++;
+        } else {
+            j++;
+        }
+    } 
+    *returnSize = k;
+    return result;
+    
+}
