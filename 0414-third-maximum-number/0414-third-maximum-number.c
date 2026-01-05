96469df50254d7f3d710f5475dc69d70daf2a820
kriti kukreja <kritikukreja6@gmail.com>
2026-01-05 14:38:54 +0530

    Time: 0 ms (100%), Space: 8.3 MB (72.39%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.c
+++ 0414-third-maximum-number/0414-third-maximum-number.c
@@ -1,27 +1,23 @@
 int thirdMax(int* nums, int numsSize) {
-    long first = LONG_MIN;
-    long second = LONG_MIN;
-    long third = LONG_MIN;
+    long long first = LLONG_MIN;
+    long long second = LLONG_MIN;
+    long long third = LLONG_MIN;
 
-    for (int i = 0 ; i < numsSize ; i++) {
+    for (int i = 0; i < numsSize ; i++) {
         int current = nums[i];
-        if (current == first || current == second ||current == third) {
-          continue;
+        if(current == first || current == second || current == third) {
+            continue;
         }
         if (current > first) {
             third = second;
             second = first;
             first = current;
-        } else if ( current > second) {
+        } else if (current > second) {
             third = second;
             second = current;
-        } else if (current > third) {
+        } else if (current > third)  {
             third = current;
         }
     }
-    if ( third == LONG_MIN) {
-        return (int)first;
-    }
-    return (int)third;
-    
+    return (third == LLONG_MIN) ? (int)first : (int)third;
 }
