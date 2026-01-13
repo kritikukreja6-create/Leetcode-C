67e4c8a8f42d4b5c593269a7aac58816193b636c
kriti kukreja <kritikukreja6@gmail.com>
2026-01-13 06:39:18 +0530

    Time: 4 ms (1.99%), Space: 15.2 MB (48.01%) - LeetHub

---
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.c
@@ -0,0 +1,26 @@
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** matrixReshape(int** mat, int matSize, int* matColSize, int r, int c, int* returnSize, int** returnColumnSizes) {
+    int m = matSize;
+    int n = matColSize[0];
+
+    if(m * n != r * c) {
+        *returnSize =  m;
+        *returnColumnSizes = matColSize;
+        return mat;
+    }
+    int** res = (int**)malloc(r * sizeof(int*));
+    *returnColumnSizes = (int*)malloc(r * sizeof(int));
+    *returnSize = r;
+    for(int i = 0 ; i < r ; i++) {
+        res[i] = (int*)malloc(c * sizeof(int));
+        (*returnColumnSizes)[i] = c;
+             }
+    for(int i = 0; i < m * n ; i++) {
+        res[i / c][i % c] = mat[i / n][i % n];
+    }
+    return res;
+}
