1b6d60169894eddfc4d154d36ab2cb33b9ac846d
kriti kukreja <kritikukreja6@gmail.com>
2026-01-16 20:56:12 +0530

    Time: 8 ms (6.79%), Space: 16.4 MB (59.41%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.c
@@ -0,0 +1,15 @@
+double findMaxAverage(int* nums, int numsSize, int k) {
+    double currentSum = 0;
+    for(int i = 0 ; i < k ; i++) {
+        currentSum += nums[i];
+    }
+    double maxSum = currentSum;
+
+    for(int i = k ; i < numsSize ; i++) {
+         currentSum += nums[i] - nums[i - k];
+         if (currentSum > maxSum) {
+            maxSum = currentSum;
+         }
+    }
+    return maxSum / k ;
+}
