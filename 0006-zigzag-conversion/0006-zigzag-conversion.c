b9c544357bb1d3471396b87a18e6dbbcb146b538
kriti kukreja <kritikukreja6@gmail.com>
2026-01-19 20:21:18 +0530

    Time: 0 ms (100%), Space: 8.8 MB (73.54%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.c
@@ -0,0 +1,20 @@
+char* convert(char* s, int numRows) {
+    int len = strlen(s);
+    if ( numRows <= 1 || numRows >= len) {
+        return s;
+    }
+    char* result = (char*)malloc((len + 1) * sizeof(char));
+ int k = 0;
+ int cycleLen = 2 * numRows - 2;
+ for (int i = 0; i < numRows ; i++) {
+    for(int j = 0; j + i < len ; j += cycleLen) {
+        result[k++] = s[j + i];
+        int diagonalIdx = j + cycleLen - i;
+        if (i != 0 && i != numRows - 1 && diagonalIdx < len) {
+            result[k++] = s[diagonalIdx];
+        }
+    }
+ }
+ result[k] = '\0';
+ return result;
+}
