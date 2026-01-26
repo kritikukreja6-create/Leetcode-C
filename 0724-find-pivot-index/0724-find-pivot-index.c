4704c44ac0f09c0c73dc131948d95f7f3affe1cf
kriti kukreja <kritikukreja6@gmail.com>
2026-01-26 13:48:04 +0530

    Time: 1 ms (27.05%), Space: 9.6 MB (29.72%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.c
@@ -0,0 +1,15 @@
+int pivotIndex(int* nums, int numsSize) {
+    int totalSum = 0;
+    int leftSum = 0;
+    
+    for (int i = 0 ; i < numsSize ; i++) {
+        totalSum += nums[i];
+    }
+    for (int i = 0 ; i < numsSize ; i++) {
+        if (leftSum * 2 == totalSum - nums[i]) {
+            return i;
+        }
+        leftSum += nums[i];
+    }
+  return -1;
+}
